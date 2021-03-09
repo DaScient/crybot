c3a2a96efe827d8c28d8341df8e3c57bc64ca9e6
donut.ipynb <25983786+Donifide@users.noreply.github.com>
2021-03-08 21:33:44 -0800

    Add files via upload

---
+++ robin_stocks/config.py
@@ -0,0 +1,20 @@
+#Enter your robinhood username and password here, then save this file in the same directory as a regular python file
+rh_password="Ldiiean3ovc4"
+rh_username="dascient@gmail.com"
+
+
+#TD Ameritrade API
+client_id="EZE9YNASXSZBWYRCD0ZHV7NV649YGE5O"
+
+
+#Nomics API
+#https://api.nomics.com/
+
+nomics_key = "1d33ebd99d6ecff0ad291ddcb047cf6f"
+nomics_email = "dascient@gmail.com"
+
+# change to false to run frfr
+debug=True
+
+# set to show plot; if set to false plot will not show
+plot=True
